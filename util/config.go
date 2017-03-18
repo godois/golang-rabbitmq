30154783a1f9bda34947bfe6f29440e5870d63a0
Marcio Godoi <souzagodoi@gmail.com>
2017-03-18 00:47:54 -0300

    Adding config.go which will be used to load file configurations

--- util/config.go
+++ util/config.go
@@ -1,5 +1,29 @@
 package util
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+var C *viper.Viper
+
+func init() {
+	C = viper.New()
+}
+
 func LoadConfig() {
+	C.SetConfigType("toml")
+	C.SetConfigName("config")    // name of config file (without extension)
+	C.AddConfigPath("../config") // call multiple times to add many search paths
+	C.AddConfigPath(".")         // optionally look for config in the working directory
+	C.AutomaticEnv()             // read in environment variables that match
+
+	if err := C.ReadInConfig(); err != nil {
+		log.Fatal(fmt.Errorf("Fatal error config file: %s \n", err))
+	} else {
+		log.Print("[Golang-RabbitMQ - Info] - Configuration file has loaded SUCCESSFULY  ...")
+	}
 
 }
